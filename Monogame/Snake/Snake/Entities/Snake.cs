b1c9826f3b2de2fd896d4a024ac9bfe1d3cb93cc
Fer VT <fer.vargas.torres@gmail.com>
2021-03-07 00:09:04 -0600

    Add basic logic for Snake WIP

---
+++ Monogame/Snake/Snake/Entities/Snake.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Snake {
+    public class Snake {
+        private Vector2 _headPosition;
+        private Texture2D _texture;
+
+        private enum Direction { 
+            Left,
+            Right,
+            Up,
+            Down
+        };
+
+        private static Random _random = new Random();
+
+        private Direction _direction;
+        private float _timeUntilNextStep;
+        private float _timeSincePreviousStep;
+
+        private List<Vector2> _body;
+
+        public Snake(Vector2 position) {
+            _headPosition = position;
+        }
+
+        public void Initialize() {
+            _direction = GetRandomDirection();
+
+            _timeUntilNextStep = 0.100f;
+            _timeSincePreviousStep = 0;
+
+            _body = new List<Vector2>() { _headPosition };
+        }
+
+        public void LoadContent(ContentManager contentManager) {
+            _texture = contentManager.Load<Texture2D>("Body Block/Body Block");
+
+            Vector2 currentPosition = _headPosition;
+            for (int i = 0; i < 3; i++) {
+                switch (_direction) {
+                    case Direction.Right:
+                        currentPosition -= new Vector2(_texture.Width, 0);
+                        break;
+
+                    case Direction.Left:
+                        currentPosition += new Vector2(_texture.Width, 0);
+                        break;
+
+                    case Direction.Up:
+                        currentPosition += new Vector2(0, _texture.Height);
+                        break;
+
+                    case Direction.Down:
+                        currentPosition -= new Vector2(0, _texture.Height);
+                        break;
+                }
+
+                _body.Add(currentPosition);
+            }
+        }
+
+        public void Update(float delta) {
+            KeyboardState state = Keyboard.GetState();
+
+            if (state.IsKeyDown(Keys.Left) && _direction != Direction.Right)
+                _direction = Direction.Left;
+
+            if (state.IsKeyDown(Keys.Right) && _direction != Direction.Left)
+                _direction = Direction.Right;
+
+            if (state.IsKeyDown(Keys.Up) && _direction != Direction.Down)
+                _direction = Direction.Up;
+
+            if (state.IsKeyDown(Keys.Down) && _direction != Direction.Up)
+                _direction = Direction.Down;
+
+            _timeSincePreviousStep += delta;
+            if (_timeSincePreviousStep < _timeUntilNextStep)
+                return;
+
+            _timeSincePreviousStep = 0;
+
+            switch (_direction) {
+                case Direction.Right:
+                    _headPosition += new Vector2(_texture.Width, 0);
+                    break;
+
+                case Direction.Left:
+                    _headPosition -= new Vector2(_texture.Width, 0);
+                    break;
+
+                case Direction.Up:
+                    _headPosition -= new Vector2(0, _texture.Height);
+                    break;
+
+                case Direction.Down:
+                    _headPosition += new Vector2(0, _texture.Height);
+                    break;
+            }
+
+            UpdateBodyBlockPosition(0, _headPosition);
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            foreach (Vector2 bodyBlock in _body)
+                spriteBatch.Draw(_texture, bodyBlock, Color.White);
+        }
+
+        private Direction GetRandomDirection() {
+            Array values = Enum.GetValues(typeof(Direction));
+            return (Direction)values.GetValue(_random.Next(values.Length));
+        }
+
+        public bool IsPositionInBody(Vector2 position) {
+            foreach (Vector2 bodyBlock in _body)
+                if (position.Equals(bodyBlock))
+                    return true;
+
+            return false;
+        }
+
+        private void UpdateBodyBlockPosition(int index, Vector2 newPosition) {
+            if (index >= _body.Count)
+                return;
+
+            UpdateBodyBlockPosition(index + 1, _body[index]);
+            _body[index] = newPosition;
+        }
+    }
+}
---
+++ Monogame/Snake/Snake/SnakeGame.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Snake {
+    public class SnakeGame : Game {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Vector2 _screenSize;
+
+        private Snake _snake;
+
+        private Vector2 _foodPosition;
+        private Texture2D _foodTexture;
+
+        public SnakeGame() {
+            _graphics = new GraphicsDeviceManager(this);
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize() {
+            _screenSize = new Vector2(1024, 576);
+
+            _graphics.PreferredBackBufferWidth = (int)_screenSize.X;
+            _graphics.PreferredBackBufferHeight = (int)_screenSize.Y;
+            _graphics.ApplyChanges();
+
+            _snake = new Snake(new Vector2(_screenSize.X / 2, _screenSize.Y / 2));
+            _snake.Initialize();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent() {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _snake.LoadContent(Content);
+            _foodTexture = Content.Load<Texture2D>("Food/Food");
+
+            SetRandomFoodPosition();
+        }
+
+        protected override void Update(GameTime gameTime) {
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            _snake.Update((float)gameTime.ElapsedGameTime.TotalSeconds);
+
+            if (_snake.IsPositionInBody(_foodPosition))
+                SetRandomFoodPosition();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.Black);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(_foodTexture, _foodPosition, Color.White);
+            _snake.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void SetRandomFoodPosition() {
+            Random random = new Random();
+
+            _foodPosition = new Vector2(random.Next(0, (int)_screenSize.X), random.Next(0, (int)_screenSize.Y));
+            while (_snake.IsPositionInBody(_foodPosition))
+                _foodPosition = new Vector2(random.Next(0, (int)_screenSize.X), random.Next(0, (int)_screenSize.Y));
+        }
+    }
+}
